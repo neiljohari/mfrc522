97bba68f7a647703159713f3447c11bbe6e45155
Neil Johari <neil@johari.tech>
2019-08-06 17:48:45 -0400

    Implement real anticollision
    Begin implementing real anticollision with sendSEL
    
    Remove old anticollision method
    
    Merge backData into uidBuffer correctly
    
    Enable checksum on any 5 byte return from sendSEL
    
    Initialize SAK to 0
    
    Handle SAK as a pointer, remove debug
    
    Display BCC for targeted ID
    
    Only write what is necessary into cmdFrame
    
    Fix BCC being calculated incorrectly for partially sent Uids
    
    Capitalize NVB everywhere, add more comments
    
    Use statically defined array sizes

--- rfid/rfid/rfid.ino
+++ rfid/rfid/rfid.ino
@@ -117,21 +117,26 @@ void setup() {
 
 void loop() {
   if(isNewCardPresent()) {
-    byte serNum[5];
+    byte serNum[5] = {0x00};
 
-    StatusCode anticollisionStatus = performAnticollision(serNum);
+    StatusCode anticollisionStatus = performAnticollision(PICC_SEL_CL1, serNum);
 
     if(anticollisionStatus == STATUS_OK) {
 
       char uid[11];
       sprintf(uid,"%02X:%02X:%02X:%02X", serNum[0], serNum[1], serNum[2], serNum[3]);
       Serial.println("UID of card targeted: " + String(uid));
+      Serial.print("The BCC for the UID targeted is "); Serial.println(serNum[4], BIN);
 
-      byte sak;
-      selectCard(serNum, sak);
+      byte sak = 0;
+      StatusCode selectStatus = selectCard(serNum, &sak);
 
       TagType cardType = getTagType(sak);
-
+      
+      if(selectStatus != STATUS_OK) {
+        Serial.print("selectCard failed. Error code "); Serial.println(selectStatus);
+      }
+      
       if(cardType == PICC_TYPE_MIFARE_1K) {
         Serial.println("Attempting Mifare Classic Operation (MIFARE Read)");
 
@@ -163,12 +168,15 @@ void loop() {
           sendHLTA(); 
         }
       }
+    } else {
+      Serial.print("Anticollision failed with status code: "); Serial.println(anticollisionStatus);
     }
   }
 
   stopEncryptedCommunication();
 
-  delay(500);
+  Serial.println();
+  delay(2000);
 }
 
 void initReader() {
@@ -471,15 +479,14 @@ StatusCode executeDataCommand(byte cmd, byte successIrqFlag,
   //  b7 .  b6 .    b5 .     b4 .       b3 .    b2 .   b1 .      b0
   if(readReg(ErrorReg) & B00010011) // BufferOvfl, ParityErr, and ProtocolErr
     return STATUS_ERROR;
-
+  
   if (backData && backLen) {
     byte fifoByteCount = readReg(FIFOLevelReg); 
-
+    
     if (fifoByteCount > *backLen) 
       return STATUS_NO_ROOM;
-    
+      
     *backLen = fifoByteCount;
-
     readFIFOData(fifoByteCount, backData, rxAlign);
 
    
@@ -565,37 +572,41 @@ StatusCode sendHLTA() {
  *  - nvb: Number Valid Bits encoding as described by the ISO/IEC 1444-3
  *  - sendData: any number of bits of UID you wish to send
  * outputs:
- *  - backData: data returned from PICC. Could be either the remaining bits of a UID for a PICC, or a SAK + CRC_A
+ *  - backData: data returned from PICC. Could be either the remaining bits of a UID for a PICC + BCC, or a SAK + CRC_A
  *  - backLen: length in bytes of data returned
  *  - validReturnBits: number of valid bits in last byte returned
  */
-StatusCode sendSEL(byte cascadeCommand, byte nvb, byte *sendData, byte *backData, byte *backLen, byte *validReturnBits) {
+StatusCode sendSEL(byte cascadeCommand, byte NVB, byte *sendData, byte *backData, byte *backLen, byte *validReturnBits) {
 
   // NVB description:
   //  nvbByteCount: "The upper 4 bits are called “Byte count” and specify the integer part of the number of all valid data bits transmitted
   //   by the PCD (including SEL and NVB) divided by 8" (ISO/IEC 1444-3 6.4.3.3)
+  //   Note: b8 isn't used in NVB 
   //  nvbBitCount: "The lower 4 bits are called “bit count” and specify the number of all valid data bits transmitted by the PCD
   //   (including SEL and NVB) modulo 8."
-  const byte nvbByteCount = (nvb & B11110000) >> 4;
-  const byte nvbBitCount = (nvb & B00001111);
+  //   Note: b4 isn't used in NVB
+  const byte nvbByteCount = (NVB & B01110000) >> 4;
+  const byte nvbBitCount = (NVB & B00000111);
   const byte totalKnownBitsForCascadeLevel = nvbByteCount * 8 + nvbBitCount;
 
-  // nvbByteCount accounts for SEL and NVB (so it is always >= 2) and also the integer part of all valid bits transmitted
-  //  additionally, if there are some bits in the last byte being sent, we'll need to allocate another byte
-  //  additionally, if we are sending the whole UID we will need another 2 bytes for the CRC_A
-  const byte cmdBufferSize = nvbByteCount + (nvbBitCount == 0 ? 0 : 1) + (nvb == 0x70 ? 2 : 0);
-
-  byte cmdFrame[cmdBufferSize]; 
+  // We need 2 bytes for SEL and NVB
+  //  Then we need bytes for the whole UID. This is nvbByteCount and then an extra byte for any bits after the last whole byte.
+  //  additionally, if we are sending the whole UID we will need another 2 bytes for the CRC_A 
+  const uint8_t bytesForWholeUid = (nvbByteCount - 2) + (nvbBitCount == 0 ? 0 : 1); // We subtract 2 because NVB accounts for SEL and NVB itself
+  const uint8_t cmdBufferSize = 2 + bytesForWholeUid + (NVB == 0x70 ? 2 : 0); 
   
+  // The buffer can be at most 9 bytes long: SEL (1) + NVB (1) + UID (1-4) + BCC (0-1) + CRC_A (2)
+  // Not all of this will always be used though depending on UID length
+  byte cmdFrame[9]; 
   cmdFrame[0] = cascadeCommand; // SEL command for corresponding cascade level
-  cmdFrame[1] = nvb; // NVB. See ISO/IEC 14443-3 6.4.3.3 for "Coding of NVB"
+  cmdFrame[1] = NVB; // NVB. See ISO/IEC 14443-3 6.4.3.3 for "Coding of NVB"
 
   // Send the known portion of the Uid
-  for(int i = 0 ; i < cmdBufferSize ; i++) {
+  for(int i = 0 ; i < bytesForWholeUid ; i++) {
     cmdFrame[i+2] = sendData[i];
   }
 
-  if(nvb == 0x70) {
+  if(NVB == 0x70) {
    uint8_t crcDataSize = cmdBufferSize - 2;
    // A CRC is computed for all data bits currently in the frame
    // The result is stored in the last 2 bytes in our command frame
@@ -608,6 +619,7 @@ StatusCode sendSEL(byte cascadeCommand, byte nvb, byte *sendData, byte *backData
   uint8_t validBits = nvbBitCount; // Transmit only the number of bits specified by NVB in the last byte of sendUid
 
   clearLoggedCollisionBits();
+    
   StatusCode status = executeDataCommand(Transceive, B00110000, cmdFrame, cmdBufferSize, backData, backLen, &validBits, totalKnownBitsForCascadeLevel % 8);
   *validReturnBits = validBits;
   
@@ -617,19 +629,27 @@ StatusCode sendSEL(byte cascadeCommand, byte nvb, byte *sendData, byte *backData
   if(~collisions & B00100000) { // If b5 is logic 0, a collision was detected
     return STATUS_COLLISION;
   }
-  
+
   if(status == STATUS_OK && backLen && *backLen == 5) {
+    byte mask = 0xFF << nvbBitCount;
+    
     // "UID CLn check byte, calculated as exclusive-or over the 4 previous bytes, Type A" (ISO/IEC 1444-3 4)
-    byte byte0 = backData[0];
+    // The BCC is not recalculated for partially sent Uid bytes, so for byte0 we merge in the portion of the UID sent from cmdFrame
+    //    More info on this: cmdFrame index 0 and 1 are SEL and NVB, then the following indices are UID bytes. 1 + bytesForWholeUid gets the last byte with bits being sent.
+    //                       The mask takes only the bits actually being sent and merges it with bits from first Uid byte coming in
+    byte byte0 = (cmdFrame[1 + bytesForWholeUid] & ~mask) | (backData[0] & mask); 
     byte byte1 = backData[1];
     byte byte2 = backData[2];
     byte byte3 = backData[3];
     byte BCC   = backData[4];
     
-    byte checksum = byte0 ^ byte1 ^ byte2 ^ byte3;
+    byte checksum = ((byte0 ^ byte1) ^ byte2) ^ byte3;
 
-    if(checksum != BCC)
+    if(checksum != BCC) {
+      Serial.println("FAILED CHECKSUM");
+      Serial.print("Got: "); Serial.print(checksum, BIN); Serial.print(", expected:"); Serial.println(BCC, BIN);
       return STATUS_ERROR;
+    }
   }
   
   return status;
@@ -664,23 +684,75 @@ TagType getTagType(byte SAK) {
   return PICC_TYPE_UNKNOWN;
 }
 
-/*
- * This is a very basic naive implementation of anticollision.
- * 
- * It does not handle bit collisions, nor does it escalate cascade levels to retrieve a UID larger than type single. 
- * Additionally, it does not verify that we have received the SAK (Select AcKnowledge) frame which would indicate a full UID.
- */
-StatusCode performAnticollision(byte *serialNumber) {
-  byte validReturnBits = 0;
-  byte backLen = 5;
+StatusCode performAnticollision(byte cascadeCommand, byte *uidBuffer) {
+  bool collision = false;
+  uint8_t collisionPosition = 0; // This is the bit within this cascade level that a collision occurred
+  byte NVB = 0x20; // Start off with minimum valid bits
   
-  return sendSEL(PICC_SEL_CL1, 0x20, serialNumber, serialNumber, &backLen, &validReturnBits);
+  do {
+    if (collision) {
+      Serial.println("**Collision detected. Attempting to resolve it.**");
+      
+      if (readReg(CollReg) & B00100000) { // CollPosNotValid
+        Serial.println("CollPos not valid. Aborting this anticollision.");
+        return STATUS_COLLISION; 
+      }
+      
+      collisionPosition = readReg(CollReg) & B00011111; // The CollPos is b4..b0, this handles bit values 0-31
+      if(collisionPosition == 0x00) collisionPosition = 32; // bit collision at 32nd bit is represented as 0x00
+
+      Serial.print("A collision was detected at this bit: "); Serial.println(collisionPosition);
+      
+      // There are 4 UID bytes for a given cascade level. Since integral division is truncation, this gives us a zero-indexed 
+      //  number representing the whole number of bytes needed to represent this collision
+      byte collisionByte = collisionPosition / 8; 
+      // This is the actual bit within the above specified byte that has collided, it is one-indexed
+      byte collisionBitWithinByte = collisionPosition % 8; 
+  
+      // Now we craft an NVB code using ISO/IEC 14443-3 6.4.3.3 Table 7
+      NVB += (collisionByte << 4) + collisionBitWithinByte;
+      Serial.print("Our NVB frame is now 0x"); Serial.println(NVB, HEX);
+
+      // We set the collided bit to 1b
+      uidBuffer[collisionByte] |= 1 << (collisionBitWithinByte-1); 
+    }
+
+
+    int knownBytes = ((NVB & B11110000) >> 4) - 2; // Subtract out 2 bytes, one for SEL and one for NVB
+      
+    byte backData[5] = {0}; // backData can be at most 5 bytes long, but we might not use all of this allocated memory
+    byte backDataLen = 5 - knownBytes;
+    byte validReturnBits;
+    StatusCode commandStatus = sendSEL(cascadeCommand, NVB, uidBuffer, backData, &backDataLen, &validReturnBits);
+
+    // sendSEL populates backData with the remaining Uid bits for CLn
+    // The following code will update uidBuffer with the new Uid bits while preserving the existing known bits
+    // This is effectively a Uid merge
+    for(int i = knownBytes ; i < 5 ; i++) {
+      if(i == knownBytes) {
+        byte mask = 0xFF << (NVB & B00001111);
+        uidBuffer[knownBytes] = (uidBuffer[knownBytes] & ~mask) | (backData[0] & mask);
+      } else {
+        uidBuffer[i] = backData[i - knownBytes];
+      }
+    }
+
+    collision = (commandStatus == STATUS_COLLISION);
+
+    if(commandStatus != STATUS_COLLISION && commandStatus != STATUS_OK) {
+      return commandStatus;
+    }
+    
+  } while (collision == true);
+ 
+
+  return STATUS_OK;
 }
 
 /*
  * Given a complete Uid, this function will transition a PICC in the READY state to ACTIVE state
  */
-StatusCode selectCard(byte *serialNumber, byte &SAK) {
+StatusCode selectCard(byte *serialNumber, byte *SAK) {
    byte validReturnBits = 0;
    uint8_t backLen = 3; // We expect the following bytes to come back: SAK (1 byte), CRC_A (2 bytes)
    byte backData[3]; // Allocate the byte array
@@ -688,11 +760,11 @@ StatusCode selectCard(byte *serialNumber, byte &SAK) {
    StatusCode status = sendSEL(PICC_SEL_CL1, 0x70, serialNumber, backData, &backLen, &validReturnBits);
    
    if(status == STATUS_OK && backLen == 3) {
-    SAK = backData[0];
+    *SAK = backData[0];
     // The SAK encodes a few states. See ISO/IEC 14443-3 6.4.3.4 Table 8 for coding of SAK
 
     // Here we are just checking to see if we have a full UID
-    if(SAK & B00000100) {
+    if(*SAK & B00000100) {
       Serial.println("Cascade bit set: UID not complete");
       return STATUS_INVALID;
     }
